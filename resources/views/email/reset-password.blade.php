7aedbd65f4c53b2fc886f7c90cfeceefabc1d152
Brendo Marinho <141501619+brendoomarinho@users.noreply.github.com>
2024-04-09 21:23:14 -0300

    .
    .

--- resources/views/email/reset-password.blade.php
+++ resources/views/email/reset-password.blade.php
@@ -1,12 +1,13 @@
 <x-mail::message>
 # Olá! Recebemos sua solicitação de recuperação de senha.
 
-Clique no botão abaixo para redefinir sua senha. Caso você não tenha solicitado, não é necessário nenhuma ação, apenas ignore este email.
+Clique no botão abaixo para redefinir sua senha. Caso você não tenha feito esta solicitação, apenas ignore este email.
 
 <x-mail::button :url="''">
 Redefinir senha
 </x-mail::button>
 
 Obrigado,<br>
-{{ config('app.name') }}
+Equipe de suporte | {{ config('app.name') }}.<br>
+{{-- {{ config('app.name') }} --}}
 </x-mail::message>
--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -5,7 +5,7 @@
 {{ config('app.name') }}
 </x-mail::header>
 </x-slot:header>
-<img src="{{ asset('build/img/var_logo/logo_suporte.svg') }}" class="logo" alt="Laravel Logo">
+<img src="{{ asset('build/img/var_logo/logo_all.svg') }}" class="logo" alt="Laravel Logo">
 {{-- Body --}}
 {{ $slot }}
 
@@ -21,7 +21,7 @@
 {{-- Footer --}}
 <x-slot:footer>
 <x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+© {{ date('Y') }} {{ config('app.name') }}. @lang('Todos os direitos reservados.')
 </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
